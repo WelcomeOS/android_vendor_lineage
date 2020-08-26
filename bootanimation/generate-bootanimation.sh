7e3fcbcee353cdd369c473bfe135dd516e022935
TipzTeam <rcheung844@gmail.com>
2020-08-26 21:38:16 +0800

    Welcome: fix animation (1/3)

--- bootanimation/generate-bootanimation.sh
+++ bootanimation/generate-bootanimation.sh
@@ -13,18 +13,13 @@ else
     IMAGEWIDTH="$WIDTH"
 fi
 
-IMAGESCALEWIDTH="$IMAGEWIDTH"
-IMAGESCALEHEIGHT=$(expr $IMAGESCALEWIDTH / 3)
-
-if [ "$HALF_RES" = "true" ]; then
-    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
-fi
-
-IMAGEHEIGHT=$(expr $IMAGEWIDTH / 3)
+#if [ "$HALF_RES" = "true" ]; then
+#    IMAGEWIDTH=$(expr $IMAGEWIDTH / 2)
+#fi
 
 RESOLUTION=""$IMAGEWIDTH"x"$IMAGEHEIGHT""
 
-for part_cnt in 0 1 2 3 4
+for part_cnt in 0 1
 do
     mkdir -p "$OUT/bootanimation/part$part_cnt"
 done
@@ -32,11 +27,11 @@ tar xfp "vendor/lineage/bootanimation/bootanimation.tar" -C "$OUT/bootanimation/
 mogrify -resize $RESOLUTION -colors 250 "$OUT/bootanimation/"*"/"*".png"
 
 # Create desc.txt
-echo "$IMAGESCALEWIDTH $IMAGESCALEHEIGHT" 60 > "$OUT/bootanimation/desc.txt"
+echo "$IMAGEWIDTH $IMAGEHEIGHT" 7 > "$OUT/bootanimation/desc.txt"
 cat "vendor/lineage/bootanimation/desc.txt" >> "$OUT/bootanimation/desc.txt"
 
 # Create audio.wav
-cp "vendor/lineage/bootanimation/boot.wav" "$OUT/bootanimation/boot.wav"
+cp "vendor/lineage/bootanimation/boot.wav" "$OUT/bootanimation/part0/boot.wav"
 
 # Create bootanimation.zip
 cd "$OUT/bootanimation"
