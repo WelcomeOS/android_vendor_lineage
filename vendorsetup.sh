1ac38fa88f955d09b745228d525c809af9511462
TipzTeam <rcheung844@gmail.com>
2020-08-28 14:25:52 +0800

    vendorsetup: Welcome Welcome

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
-for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep lineage-16.0 | awk '{printf "lineage_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+#for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/lineage-build-targets | sed -e 's/#.*$//' | grep lineage-16.0 | awk '{printf "lineage_%s-%s\n", $1, $2}')
+#do
+#    add_lunch_combo $combo
+#done
 
 add_lunch_combo lineage_arm-userdebug
 add_lunch_combo lineage_arm64-userdebug
